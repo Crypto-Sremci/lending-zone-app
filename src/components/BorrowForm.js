93b83098dfa38932d59d0298e7d565aeb74b11ef
MrLaki5 <mimar1207@gmail.com>
2024-06-01 14:55:21 +0200

    add: borrow creation of nft vault

--- src/components/BorrowForm.js
+++ src/components/BorrowForm.js
@@ -1,10 +1,42 @@
 import { useState } from "react";
 import AbiErc721 from "../contracts/ABI-ERC721.json";
 import IpfsImageComponent from "./IpfsImageComponent";
+import NftVault from "../contracts/NFTVault.json";
+import NftOracle from "../contracts/NftOracle.json";
+import Usdc from "../contracts/USDC.json";
+import Evc from "../contracts/Evc.json";
 
 
 const BorrowForm = ({web3, setErrorMessage, current_address}) => {
     const [imageUrl, setImageUrl] = useState("");
+    const [nftPrice, setNftPrice] = useState("");
+    const [nftAddress, setNftAddress] = useState("");
+    const [tokenId, setTokenId] = useState("");
+
+    const handleBorrow = async (e) => {
+        e.preventDefault();
+        
+        try{
+            // Deploying the NftVault contract
+            let deploy_contract = new web3.eth.Contract(NftVault.abi);
+            let payload = {
+                data: NftVault.bytecode,
+                arguments: [Evc.address, 
+                            nftAddress,
+                            tokenId]
+            }
+
+            let parameter = {
+                from: current_address,
+                maxFeePerGas: web3.utils.toWei('100', 'gwei'), // Example max fee per gas
+                maxPriorityFeePerGas: web3.utils.toWei('2', 'gwei') // Example max priority fee per gas
+            }
+            var instance = await deploy_contract.deploy(payload).send(parameter);
+            console.log("New vault address: " + instance.options.address);
+        }catch (error) {
+            setErrorMessage("Error while borrowing NFT");
+        }
+    }
 
     const handleSubmission = async (e) => {
         e.preventDefault();
@@ -12,25 +44,51 @@ const BorrowForm = ({web3, setErrorMessage, current_address}) => {
         var token_id = e.target[1].value;
         console.log(nft_address, token_id);
         if (web3.utils.isAddress(nft_address)) {
-            const instance = new web3.eth.Contract(
-                AbiErc721,
-                nft_address
-            );
-            var url = await instance.methods.tokenURI(token_id).call();
-            const owner = await instance.methods.ownerOf(token_id).call();
-            
-            // TODO: remove logs
-            console.log("URI: " + url);
-            console.log("Owner: " + owner);
+            try{
+                const instance = new web3.eth.Contract(
+                    AbiErc721,
+                    nft_address
+                );
+                if (!instance) {
+                    setErrorMessage("Contract not found!");
+                    return;
+                }
+                var url = await instance.methods.tokenURI(token_id).call();
+                const owner = await instance.methods.ownerOf(token_id).call();
+                
+                // Add placeholder image if nft does not have url set
+                if (url === "") {
+                    url = "https://placehold.co/600x400";
+                }
 
-            // TODO: uncomment
-            // if (owner.toLowerCase() !== current_address) {
-            //     setErrorMessage("You are not the owner of this NFT");
-            //     return;
-            // }
+                // TODO: remove logs
+                console.log("URI: " + url);
+                console.log("Owner: " + owner);
 
-            setImageUrl(url);
-            setErrorMessage("");
+                // TODO: uncomment
+                if (owner.toLowerCase() !== current_address) {
+                    setErrorMessage("You are not the owner of this NFT");
+                    return;
+                }
+
+                // Get the NFT price
+                const oracle_instance = new web3.eth.Contract(
+                    NftOracle.abi,
+                    NftOracle.address
+                );
+                const currPrice = await oracle_instance.methods.getQuote(Usdc.address, nft_address, token_id).call();
+
+                // TODO: remove logs
+                console.log("Evaluated price: " + currPrice);
+
+                setNftPrice(currPrice.toString());
+                setNftAddress(nft_address);
+                setTokenId(token_id);
+                setImageUrl(url);
+                setErrorMessage("");
+            } catch (error) {
+                setErrorMessage("Error while fetching NFT data");
+            }
         }
         else {
             setErrorMessage("Invalid NFT address");
@@ -64,8 +122,15 @@ const BorrowForm = ({web3, setErrorMessage, current_address}) => {
                 <div className="text-center opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 bg-gray-900">
                     <IpfsImageComponent input={imageUrl} />
                     <label className="block text-white-300 py-2 font-bold">
-                        Price: 0.1 ETH
+                        Price: {nftPrice} USDC
                     </label>
+                    <button id="file_button" className="bg-transparent border border-blue-500 
+                                                    text-blue-500 font-bold py-1 px-1 rounded 
+                                                    focus:outline-none focus:shadow-outline mt-4 
+                                                    transition-colors duration-300" type="submit"
+                                                    onClick={handleBorrow}>
+                        Borrow
+                    </button>
                 </div>
             }
         </div>
